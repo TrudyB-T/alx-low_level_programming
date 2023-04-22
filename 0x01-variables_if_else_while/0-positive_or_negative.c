8a02fff590fa6e9063345a70fabd9fc0e9d662de
TrudyB-T <drtrudybannerman@gmail.com>
2023-04-21 20:47:10 -0700

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+  * this determines if a number is positive or negative
+ */
 int main(void)
 {
 	int n;
@@ -14,10 +16,10 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-        else if (n = 0)	
-	{                                                                                                                                                            printf("%d is zero\n", n);
+        else if (n == 0)	
+	{                                                                                                                                                             printf("%d is zero\n", n);
 	}
-	else (n < 0);
+	else 
 	{
 	        printf("%d is negative\n", n);
 	}	
