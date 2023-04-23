84442f6601fc82dd1bb7190698d2760ee9976deb
TrudyB-T <drtrudybannerman@gmail.com>
2023-04-23 10:15:59 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main-entry point
+  *Description : 'displays lowercase alphabets from a to z except q and e'
+  * Return: always 0
+  */
+int main(void)
+{
+        int n = 97;
+
+        while (n < 123)
+	for (n; is not 101; n; is not 113)
+        {
+                putchar(n);
+                n++;
+        }
+        putchar('\n');
+        return (0);
+}
