607797ceaef8f5260200032c2195a6ba14d02f80
TrudyB-T <drtrudybannerman@gmail.com>
2023-04-25 19:55:04 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 /*
- * entry point - main
- * Description : prints out _putchar
+ * main Entry point 
+ * Description : 'prints out _putchar'
  * Return: always 0
  */
+
 int main(void)
+
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
-
-
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Description : 'print alphabet in lowercase'
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 97; n < 123; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
+
