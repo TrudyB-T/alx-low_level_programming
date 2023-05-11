4a86d5879d23c198b637f17974bc9ef9a71f5675
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-10 20:51:41 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen_recursion -  returns the length of a string.
- * @s : string 
+ * @s : string
  *
  * Return: the length of a string
  */
 
 int _strlen_recursion(char *s)
-{ 
+{
 	int l = 0;
 
 	if (*s)
