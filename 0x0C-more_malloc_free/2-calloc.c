0d009a770fdd578068fdb48943c70625ccf8e063
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-18 19:41:51 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int n = 0;
 	int o = nmemb * size;
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	s = malloc(o);
