a802fbc544b93fa489450b3927089e9316cdacba
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-21 19:19:06 -0700

    me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,4 +11,51 @@
  * Return: a pointer to the allocated memory
  */
 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int q = 0;
+	unsigned int w = 0;
+	unsigned int e = 0;
+	unsigned int r = 0;
+	char *l;
 
+		if (s1 == NULL)
+			s1 = "";
+
+	if (s2 == NULL)
+                s2 = "";
+
+	 while (s1[q])
+		 q++;
+
+	 while (s2[e])
+		e++;
+
+	if (n >= e)
+		r = q + e;
+
+	else
+	       r = q + n;
+
+	l = malloc(sizeof(char) * r + 1);
+
+	if (l == NULL)
+		return(NULL);
+
+	e = 0;
+
+	while (w < r)
+	{
+		if (w <= q)
+			l[w] = s1[w];
+
+		if (w >= q)
+		{
+			l[w] = s2[e];
+			e++;
+		}
+		q++;
+	}
+	l[w] = '\0';
+	return (l);
+}
