eab65993d0381e2584694351028314b52fe92b92
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-22 12:05:52 -0700

    dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog -  prints a struct dog
+ * @d: variable 
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("");
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef dog_t dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
