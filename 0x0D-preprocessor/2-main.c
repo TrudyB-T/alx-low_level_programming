16e9e2f856819da11016babf4273584ee54970a3
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-22 06:46:41 -0700

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
