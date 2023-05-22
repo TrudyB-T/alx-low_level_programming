6246296fa6ec0d0993756d34c18031b5d99efe69
TrudyB-T <drtrudybannerman@gmail.com>
2023-05-22 14:01:44 -0700

    dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,5 +10,14 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("");
+	{
+		printf("(nil)");
+	}
 
+	else
+	{
+		printf("Name:%s\n", (d->name));
+		printf("Age:%f\n", (d->age));
+		printf("Owner:%s\n", (d->owner));
+	}
+}
