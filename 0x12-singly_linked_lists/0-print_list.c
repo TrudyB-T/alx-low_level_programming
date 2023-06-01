f1003c21c6a534925f4f77183d477b8592059ca8
TrudyB-T <drtrudybannerman.com>
2023-06-01 16:57:49 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,16 +18,14 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
+
 			printf("[0] (nil)\n");
-		}
 
 		else
-		{
+
 			printf("[%u] %s\n", h->len, h->str);
 				h = h->next;
 				num++;
-		}
 	}
 	return (num);
 }
