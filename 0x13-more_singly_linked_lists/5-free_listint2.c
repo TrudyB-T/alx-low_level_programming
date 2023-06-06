8f030f3d0ecc8e81aaa43269481ff3e614edc1d0
TrudyB-T <drtrudybannerman.com>
2023-06-06 15:05:40 -0700

    free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,14 +15,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *q;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		q = head->next;
 		free(*head);
-		head = q;
+		*head = q;
 	}
 
 	*head = NULL;
