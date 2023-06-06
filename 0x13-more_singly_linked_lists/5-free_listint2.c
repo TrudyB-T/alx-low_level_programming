7c9fa1a2f7cb37161947bbbb1f4ae4063474e6af
TrudyB-T <drtrudybannerman.com>
2023-06-06 14:58:41 -0700

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint -  prints all the elements of a listint_t list.
+ * @head: pointer
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *q;
+
+	while (head != NULL)
+	{
+		q = head->next;
+		free(*head);
+		head = q;
+	}
+
+	*head = NULL;
+}
