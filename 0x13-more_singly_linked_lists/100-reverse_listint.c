e37b58b5efd175ffe367470e7ed15598bf1ead4f
TrudyB-T <drtrudybannerman.com>
2023-06-06 17:37:21 -0700

    reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,17 +13,17 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *q = NULL;
-	listint_t *r = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
 	while (*head != NULL)
 	{
-		r = (*head)->r;
-		(*head)->r = q;
-		q = *head;
-		*head = r;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
-	*head = q;
+	*head = prev;
 
-	return (head);
+	return (*head);
 }
