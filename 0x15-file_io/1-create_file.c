08fa2340692b78fa7c0733bc7c55a92885f6ead4
TrudyB-T <drtrudybannerman.com>
2023-06-12 19:51:50 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: string
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int g = 0;
+	int h = 0;
+	int i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (!text_content)
+	{
+		for (; text_content[i])
+			i++;
+	}
+
+	g = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	h = write(g, text_content, i);
+
+	if (g == -1 || h == -1)
+	{
+		return (-1);
+	}
+
+	close(g);
+
+	return (1);
+}
