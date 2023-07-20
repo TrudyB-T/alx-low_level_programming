863bcb7463c4195cd9d9065546da546c5a2b2c89
TrudyB-T <drtrudybannerman.com>
2023-07-20 15:54:34 -0700

    insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,75 +1,51 @@
 #include "lists.h"
-
 /**
- * get_dnodeint_at_index - returns the nth node of
- * a dlistint_t linked list.
+ * insert_dnodeint_at_index - returns the nth node of list.
  * @h: double pointer to the head of dlistint_t list
  * @idx: the index of the list where the new node should be added
  * @n: data to add
  *
  * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *prev_node = NULL;
-	dlistint_t *current = *h;
+	dlistint_t *new_node, *prev_node = NULL, *current = *h;
 	unsigned int j = 0;
 
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
 	new_node->n = n;
-
-	if (idx == 0)
+	while (current != NULL)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-		}
-		*h = new_node;
-	}
-
-	else
-	{
-		for (; j < idx && current != NULL; j++)
-		{
-			prev_node = current;
-			current = current->next;
-		}
-		
-		if (current != NULL || idx == 0)
+		if (j == idx)
 		{
 			new_node->prev = prev_node;
 			new_node->next = current;
-			
 			if (current != NULL)
-			{
 				current->prev = new_node;
-			}
-			
 			if (prev_node != NULL)
-			{
 				prev_node->next = new_node;
-			}
 			else
 			{
 				*h = new_node;
 			}
+			return (new_node);
 		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
+		prev_node = current;
+		current = current->next;
+		j++;
 	}
-	return (new_node);
+	if (idx == j)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	free(new_node);
+	return (NULL);
 }
