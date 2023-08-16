ac5c39dec9e0a0459f4bfbddd4cc8e0f9ded2467
TrudyB-T <drtrudybannerman.com>
2023-08-16 16:56:46 -0700

    read me

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -87,4 +87,6 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
+shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
 #endif
